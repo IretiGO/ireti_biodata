7663e88083a28d04202bc8ffccbc52e198786360
iretiGO <iretigeorgee@gmail.com>
2025-11-22 23:26:41 +0100

    Add biodata categorization assignment

---
+++ data_categorization.py
@@ -0,0 +1,34 @@
+users = {}
+SECRET = "stop"
+
+count = 0
+
+while count < 10:
+    name = input("Enter name (or type 'stop' to exit): ")
+
+    if name == SECRET:
+        break
+
+    age = int(input("Enter age: "))
+    gender = input("Enter gender: ")
+
+    # Determine the age category
+    if age >= 3 and age <= 12:
+        category = "child"
+    elif age >= 13 and age <= 19:
+        category = "teenager"
+    elif age >= 20 and age <= 39:
+        category = "young adult"
+    elif age >= 40 and age <= 64:
+        category = "middle aged"
+    elif age >= 65:
+        category = "senior"
+    else:
+        category = "unknown"
+
+    # Store in dictionary
+    users[name] = [age, category, gender]
+
+    count = count + 1
+
+print(users)
